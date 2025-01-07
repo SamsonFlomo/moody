0bf2a5e878e8bf1e1feb18180de191577f4e887d
SamsonFlomo <flomosamson125@gmail.com>
2025-01-07 13:14:02 +0000

    width, font-color, and logout issues fixed

--- src/components/UpdateProfileModal.jsx
+++ src/components/UpdateProfileModal.jsx
@@ -9,27 +9,34 @@ function UpdateProfileModal({
 }) {
   return (
     <div className="modal-overlay">
-      <div className="modal">
+      <div className="modal update-profile">
         <button className="close-btn" onClick={closeModal}>
           x
         </button>
         <h2>Update Profile</h2>
-        <input
+        <label for="name"> Full Name
+          <input
           id="name"
           type="text"
           value={updatedUser.name}
           onChange={handleUpdateProfileChange}
           placeholder="John Doe"
           required
-        />
-        <input
+          />
+        </label>
+        
+        
+        <label for="photoURL"> Profile Picture Link
+          <input
           id="photoURL"
           type="text"
           value={updatedUser.photoURL}
           onChange={handleUpdateProfileChange}
           placeholder="https://example.com/profile-picture.jpg"
           required
-        />
+          />
+        </label>
+        
         <button className="primary-btn" onClick={handleUpdateProfile}>
           Update
         </button>
